8a0373f50fbff4b897a0781e6408f2d99d698cfe
Toshiyuki Sakai <toshi6661024@gmail.com>
2022-07-12 23:37:10 +0900

    update

--- module_qtlseq.py
+++ module_qtlseq.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import random
 import copy
 
@@ -43,3 +46,42 @@ def make_F2_progeny(cultivar_A, cultivar_B, progeny=200):
     children.columns = ["Genotype"]
     children["Phenotype"] = phenotype
     return children
+
+def check_distribution(f2_progeny):
+    sns.set()
+    plt.hist(f2_progeny["Phenotype"])
+
+def high_and_low_bulk_sequencing(progeny, top=20, bottom=20, reads=500):
+    children_sort = progeny.sort_values(by="Phenotype")
+    high_bulk = children_sort.iloc[-1*top:, :]
+    low_bulk = children_sort.iloc[:bottom, :]
+
+    low_reads = []
+    for i in range(reads):
+        tmp = np.random.choice(low_bulk.iloc[:, 0].values)
+        start = np.random.choice(range(len(tmp)-9))
+        read = tmp[start:start+10]
+        low_reads.append([read, start])
+
+    low_reads = pd.DataFrame(low_reads)
+    low_reads.to_csv("../low_reads.csv")
+
+    with open("low_bulked_sequences.fasta", mode='w') as f:
+        for i in range(low_reads.shape[0]):
+            f.write(">read{}\n".format(i))
+            f.write("{}\n".format(low_reads.iloc[i, 0]))
+
+    high_reads = []
+    for i in range(reads):
+        tmp = np.random.choice(high_bulk.iloc[:, 0].values)
+        start = np.random.choice(range(len(tmp)-9))
+        read = tmp[start:start+10]
+        high_reads.append([read, start])
+
+    high_reads = pd.DataFrame(high_reads)
+    high_reads.to_csv("../high_reads.csv")
+
+    with open("high_bulked_sequences.fasta", mode='w') as f:
+        for i in range(high_reads.shape[0]):
+            f.write(">read{}\n".format(i))
+            f.write("{}\n".format(high_reads.iloc[i, 0]))
