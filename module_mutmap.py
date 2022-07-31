c043796d0a7c8a5e585352332eb29b3d00539696
Toshiyuki Sakai <toshi6661024@gmail.com>
2022-07-31 18:57:27 +0900

    Update module_mutmap.py

--- module_mutmap.py
+++ module_mutmap.py
@@ -151,7 +151,7 @@ def calculate_SNP_index(alignment_result, reference, mutant):
     table_df["mut_num"] = mut_num
     table_df["SNP_index"] = table_df["mut_num"] / (table_df["mut_num"] + table_df["ref_num"])
     table_df.index = table_df.index.values + 1
-    table_df = table_df[table_df["muta_num"] > 0]
+    table_df = table_df[table_df["mut_num"] > 0]
     return table_df
 
 def visualize_SNP_index(SNP_index):
