934c50d947716a189f5a25a74195aa437b52e2db
Toshiyuki Sakai <toshi6661024@gmail.com>
2022-08-04 00:33:44 +0900

    Update module_qtlseq.py

--- module_qtlseq.py
+++ module_qtlseq.py
@@ -34,7 +34,7 @@ def make_F2_progeny(cultivar_A, cultivar_B, progeny=200):
         if j != cultivar_B[i]:
             mut_pos.append(i)
 
-    mut_effect = [random.uniform(-2.5, 2.5) for i in range(len(mut_pos))]
+    mut_effect = [random.uniform(-2, 2) for i in range(len(mut_pos))]
     mut_effect[12] = 10
 
     pd.DataFrame({"Position": mut_pos, "Simulated_SNP_effect":mut_effect}).to_csv("../SNP_effect.csv")
