a06cfdc4781d015aea1642286bce6bc428b9a3e9
Toshiyuki Sakai <toshi6661024@gmail.com>
2024-03-28 17:02:08 +0900

    update bash script

--- FAO_2024/data/get_data.sh
+++ FAO_2024/data/get_data.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# read simulator
+mkdir tools; cd tools
+git clone https://github.com/lh3/wgsim.git
+cd wgsim
+gcc -g -O2 -Wall -o wgsim wgsim.c -lz -lm
+cd ../..
+
+# sample data
 mkdir genome
 cd genome
 wget https://raw.githubusercontent.com/slt666666/FAO_lecture/main/FAO_2024/data/genome/CultivarA.fa 2>/dev/null
