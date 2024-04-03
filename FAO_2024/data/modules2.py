cb849c52a5d822c777b028c690fdf1de9cd345e1
Toshiyuki Sakai <toshi6661024@gmail.com>
2024-04-03 15:05:11 +0900

    add modules2

---
+++ FAO_2024/data/modules2.py
@@ -0,0 +1,30 @@
+import os
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import ElasticNet
+sns.set()
+
+def linear_model(X, y):
+    model = ElasticNet(alpha=0.3, l1_ratio=0.7)
+    model = model.fit(X, y["Grain_number"])
+    equation = "y = "
+    k = 0
+    for i, j in enumerate(model.coef_):
+        if j == 0:
+            pass
+        else:
+            k += 1
+            equation += "SNP{}×{} + ".format(str(i+1), str(j))
+    equation += str(model.intercept_)
+    print(equation)
+    return model
+
+def check_accuracy(model, X, y):
+    y_test_preds = model.predict(X)
+    plt.scatter(y.Grain_number, y_test_preds)
+    plt.xlabel("Observed phenotype values")
+    plt.ylabel("Predicted phenotype values")
+    print("correlation coefficient: {}".format(np.corrcoef(y_test.Grain_number, y_test_preds)[0, 1]))
