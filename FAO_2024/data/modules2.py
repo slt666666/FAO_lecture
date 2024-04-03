b13cc9b851681821e49c24a4063955cd069ac66d
Toshiyuki Sakai <toshi6661024@gmail.com>
2024-04-03 16:31:22 +0900

    Update modules2.py

--- FAO_2024/data/modules2.py
+++ FAO_2024/data/modules2.py
@@ -29,3 +29,33 @@ def check_accuracy(model, X, y):
     plt.ylabel("Predicted phenotype values")
     plt.title("correlation coefficient: {}".format(np.corrcoef(y.Grain_number, y_test_preds)[0, 1]), fontsize="large")
     plt.show()
+
+def predict_progeny_phenotype(Line1, Line2, progeny, genotype, prediction_model):
+
+    print("If we cross {} & {}, the phenotype of F2 population may be...".format(Line1, Line2))
+
+    progenies = []
+
+    for k in range(progeny):
+        a = np.arange(len(genotype[Line1].values))
+        a = np.random.permutation(a)[:len(genotype[Line1].values)//100]
+        a.sort()
+
+        if np.random.rand() > 0.5:
+            new = copy.copy(genotype[Line1].values)
+            other = copy.copy(genotype[Line2].values)
+        else:
+            new = copy.copy(genotype[Line2].values)
+            other = copy.copy(genotype[Line1].values)
+
+        for i in range(len(a)//2):
+            new[a[i*2]:a[i*2+1]] = other[a[i*2]:a[i*2+1]]
+        
+        new[new < 0] = 0
+        progenies.append(new)
+    progenies_genotype = pd.DataFrame(progenies).T
+
+    pred = predict_phenotype(progenies_genotype, prediction_model)
+    sns.set()
+    plt.hist(pred)
+    plt.show()
